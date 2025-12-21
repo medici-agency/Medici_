f995146ec3e9054930b2b10c7c7d857246583669
MEDICI <moto08405@gmail.com>
2025-12-21 12:35:52 +0200

    й

--- plugins/medici-forms-pro/assets/js/builder.js
+++ plugins/medici-forms-pro/assets/js/builder.js
@@ -6,252 +6,250 @@
  */
 
 (function ($) {
-  "use strict";
+	'use strict';
 
-  /**
-   * Form Builder
-   */
-  const MediciFormsBuilder = {
-    /**
-     * Initialize
-     */
-    init: function () {
-      this.bindEvents();
-      this.initSortable();
-      this.initTabs();
-    },
+	/**
+	 * Form Builder
+	 */
+	const MediciFormsBuilder = {
+		/**
+		 * Initialize
+		 */
+		init: function () {
+			this.bindEvents();
+			this.initSortable();
+			this.initTabs();
+		},
 
-    /**
-     * Bind events
-     */
-    bindEvents: function () {
-      // Add field
-      $(document).on("click", ".mf-add-field", this.addField.bind(this));
+		/**
+		 * Bind events
+		 */
+		bindEvents: function () {
+			// Add field
+			$(document).on('click', '.mf-add-field', this.addField.bind(this));
 
-      // Remove field
-      $(document).on("click", ".mf-remove-field", this.removeField.bind(this));
+			// Remove field
+			$(document).on('click', '.mf-remove-field', this.removeField.bind(this));
 
-      // Toggle field options
-      $(document).on("click", ".mf-toggle-field", this.toggleField);
+			// Toggle field options
+			$(document).on('click', '.mf-toggle-field', this.toggleField);
 
-      // Duplicate field
-      $(document).on(
-        "click",
-        ".mf-duplicate-field",
-        this.duplicateField.bind(this)
-      );
+			// Duplicate field
+			$(document).on('click', '.mf-duplicate-field', this.duplicateField.bind(this));
 
-      // Update field label in header
-      $(document).on("input", ".mf-field-label-input", this.updateFieldLabel);
+			// Update field label in header
+			$(document).on('input', '.mf-field-label-input', this.updateFieldLabel);
 
-      // Copy shortcode
-      $(document).on("click", "#mf-copy-shortcode", this.copyShortcode);
-      $(document).on("click", ".mf-shortcode-copy", this.copyShortcode);
-    },
+			// Copy shortcode
+			$(document).on('click', '#mf-copy-shortcode', this.copyShortcode);
+			$(document).on('click', '.mf-shortcode-copy', this.copyShortcode);
+		},
 
-    /**
-     * Initialize sortable
-     */
-    initSortable: function () {
-      $(".mf-fields-list").sortable({
-        handle: ".mf-field-drag",
-        placeholder: "ui-sortable-placeholder",
-        opacity: 0.7,
-        update: function () {
-          MediciFormsBuilder.updateFieldIndexes();
-        },
-      });
-    },
+		/**
+		 * Initialize sortable
+		 */
+		initSortable: function () {
+			$('.mf-fields-list').sortable({
+				handle: '.mf-field-drag',
+				placeholder: 'ui-sortable-placeholder',
+				opacity: 0.7,
+				update: function () {
+					MediciFormsBuilder.updateFieldIndexes();
+				},
+			});
+		},
 
-    /**
-     * Initialize tabs
-     */
-    initTabs: function () {
-      $(".mf-tab-button").on("click", function () {
-        const tab = $(this).data("tab");
+		/**
+		 * Initialize tabs
+		 */
+		initTabs: function () {
+			$('.mf-tab-button').on('click', function () {
+				const tab = $(this).data('tab');
 
-        // Update buttons
-        $(".mf-tab-button").removeClass("active");
-        $(this).addClass("active");
+				// Update buttons
+				$('.mf-tab-button').removeClass('active');
+				$(this).addClass('active');
 
-        // Update content
-        $(".mf-tab-content").removeClass("active");
-        $('.mf-tab-content[data-tab="' + tab + '"]').addClass("active");
-      });
-    },
+				// Update content
+				$('.mf-tab-content').removeClass('active');
+				$('.mf-tab-content[data-tab="' + tab + '"]').addClass('active');
+			});
+		},
 
-    /**
-     * Add new field
-     */
-    addField: function (e) {
-      e.preventDefault();
+		/**
+		 * Add new field
+		 */
+		addField: function (e) {
+			e.preventDefault();
 
-      const $button = $(e.currentTarget);
-      const type = $button.data("type");
-      const $list = $(".mf-fields-list");
-      const index = $list.find(".mf-field-item").length;
+			const $button = $(e.currentTarget);
+			const type = $button.data('type');
+			const $list = $('.mf-fields-list');
+			const index = $list.find('.mf-field-item').length;
 
-      // Remove empty state
-      $list.find(".mf-empty-state").remove();
+			// Remove empty state
+			$list.find('.mf-empty-state').remove();
 
-      // AJAX request to get field HTML
-      $.ajax({
-        url: mediciFormsBuilder.ajaxUrl,
-        type: "POST",
-        data: {
-          action: "medici_forms_add_field",
-          nonce: mediciFormsBuilder.nonce,
-          type: type,
-          index: index,
-        },
-        success: function (response) {
-          if (response.success) {
-            const $field = $(response.data.html);
-            $list.append($field);
+			// AJAX request to get field HTML
+			$.ajax({
+				url: mediciFormsBuilder.ajaxUrl,
+				type: 'POST',
+				data: {
+					action: 'medici_forms_add_field',
+					nonce: mediciFormsBuilder.nonce,
+					type: type,
+					index: index,
+				},
+				success: function (response) {
+					if (response.success) {
+						const $field = $(response.data.html);
+						$list.append($field);
 
-            // Open field options
-            $field.find(".mf-field-content").slideDown();
-            $field.find(".mf-toggle-field .dashicons").removeClass("dashicons-arrow-down-alt2").addClass("dashicons-arrow-up-alt2");
+						// Open field options
+						$field.find('.mf-field-content').slideDown();
+						$field
+							.find('.mf-toggle-field .dashicons')
+							.removeClass('dashicons-arrow-down-alt2')
+							.addClass('dashicons-arrow-up-alt2');
 
-            // Focus on label input
-            $field.find(".mf-field-label-input").focus();
-          }
-        },
-      });
-    },
+						// Focus on label input
+						$field.find('.mf-field-label-input').focus();
+					}
+				},
+			});
+		},
 
-    /**
-     * Remove field
-     */
-    removeField: function (e) {
-      e.preventDefault();
-      e.stopPropagation();
+		/**
+		 * Remove field
+		 */
+		removeField: function (e) {
+			e.preventDefault();
+			e.stopPropagation();
 
-      if (!confirm(mediciFormsBuilder.i18n.confirmDelete)) {
-        return;
-      }
+			if (!confirm(mediciFormsBuilder.i18n.confirmDelete)) {
+				return;
+			}
 
-      const $field = $(e.currentTarget).closest(".mf-field-item");
-      $field.slideUp(200, function () {
-        $(this).remove();
-        MediciFormsBuilder.updateFieldIndexes();
+			const $field = $(e.currentTarget).closest('.mf-field-item');
+			$field.slideUp(200, function () {
+				$(this).remove();
+				MediciFormsBuilder.updateFieldIndexes();
 
-        // Show empty state if no fields
-        if ($(".mf-fields-list .mf-field-item").length === 0) {
-          $(".mf-fields-list").html(
-            '<div class="mf-empty-state">' +
-              '<span class="dashicons dashicons-plus-alt2"></span>' +
-              "<p>Додайте поля з панелі зліва</p>" +
-              "</div>"
-          );
-        }
-      });
-    },
+				// Show empty state if no fields
+				if ($('.mf-fields-list .mf-field-item').length === 0) {
+					$('.mf-fields-list').html(
+						'<div class="mf-empty-state">' +
+							'<span class="dashicons dashicons-plus-alt2"></span>' +
+							'<p>Додайте поля з панелі зліва</p>' +
+							'</div>'
+					);
+				}
+			});
+		},
 
-    /**
-     * Toggle field options
-     */
-    toggleField: function (e) {
-      e.preventDefault();
-      e.stopPropagation();
+		/**
+		 * Toggle field options
+		 */
+		toggleField: function (e) {
+			e.preventDefault();
+			e.stopPropagation();
 
-      const $field = $(this).closest(".mf-field-item");
-      const $content = $field.find(".mf-field-content");
-      const $icon = $(this).find(".dashicons");
+			const $field = $(this).closest('.mf-field-item');
+			const $content = $field.find('.mf-field-content');
+			const $icon = $(this).find('.dashicons');
 
-      $content.slideToggle(200);
-      $icon.toggleClass("dashicons-arrow-down-alt2 dashicons-arrow-up-alt2");
-    },
+			$content.slideToggle(200);
+			$icon.toggleClass('dashicons-arrow-down-alt2 dashicons-arrow-up-alt2');
+		},
 
-    /**
-     * Duplicate field
-     */
-    duplicateField: function (e) {
-      e.preventDefault();
-      e.stopPropagation();
+		/**
+		 * Duplicate field
+		 */
+		duplicateField: function (e) {
+			e.preventDefault();
+			e.stopPropagation();
 
-      const $field = $(e.currentTarget).closest(".mf-field-item");
-      const $clone = $field.clone();
-      const $list = $(".mf-fields-list");
+			const $field = $(e.currentTarget).closest('.mf-field-item');
+			const $clone = $field.clone();
+			const $list = $('.mf-fields-list');
 
-      // Generate new ID
-      const newId =
-        "field_" + Math.random().toString(16).substring(2, 10);
-      $clone.find('input[name*="[id]"]').val(newId);
+			// Generate new ID
+			const newId = 'field_' + Math.random().toString(16).substring(2, 10);
+			$clone.find('input[name*="[id]"]').val(newId);
 
-      // Update label
-      const currentLabel = $clone.find(".mf-field-label-input").val();
-      $clone.find(".mf-field-label-input").val(currentLabel + " (копія)");
-      $clone.find(".mf-field-label").text(currentLabel + " (копія)");
+			// Update label
+			const currentLabel = $clone.find('.mf-field-label-input').val();
+			$clone.find('.mf-field-label-input').val(currentLabel + ' (копія)');
+			$clone.find('.mf-field-label').text(currentLabel + ' (копія)');
 
-      // Close options
-      $clone.find(".mf-field-content").hide();
-      $clone
-        .find(".mf-toggle-field .dashicons")
-        .removeClass("dashicons-arrow-up-alt2")
-        .addClass("dashicons-arrow-down-alt2");
+			// Close options
+			$clone.find('.mf-field-content').hide();
+			$clone
+				.find('.mf-toggle-field .dashicons')
+				.removeClass('dashicons-arrow-up-alt2')
+				.addClass('dashicons-arrow-down-alt2');
 
-      // Insert after current field
-      $field.after($clone);
+			// Insert after current field
+			$field.after($clone);
 
-      // Update indexes
-      this.updateFieldIndexes();
-    },
+			// Update indexes
+			this.updateFieldIndexes();
+		},
 
-    /**
-     * Update field label in header
-     */
-    updateFieldLabel: function () {
-      const label = $(this).val();
-      const $field = $(this).closest(".mf-field-item");
-      $field.find(".mf-field-label").text(label);
-    },
+		/**
+		 * Update field label in header
+		 */
+		updateFieldLabel: function () {
+			const label = $(this).val();
+			const $field = $(this).closest('.mf-field-item');
+			$field.find('.mf-field-label').text(label);
+		},
 
-    /**
-     * Update field indexes after reordering
-     */
-    updateFieldIndexes: function () {
-      $(".mf-fields-list .mf-field-item").each(function (index) {
-        $(this).attr("data-index", index);
+		/**
+		 * Update field indexes after reordering
+		 */
+		updateFieldIndexes: function () {
+			$('.mf-fields-list .mf-field-item').each(function (index) {
+				$(this).attr('data-index', index);
 
-        // Update all input names
-        $(this)
-          .find("input, select, textarea")
-          .each(function () {
-            const name = $(this).attr("name");
-            if (name) {
-              const newName = name.replace(
-                /medici_form_fields\[\d+\]/,
-                "medici_form_fields[" + index + "]"
-              );
-              $(this).attr("name", newName);
-            }
-          });
-      });
-    },
+				// Update all input names
+				$(this)
+					.find('input, select, textarea')
+					.each(function () {
+						const name = $(this).attr('name');
+						if (name) {
+							const newName = name.replace(
+								/medici_form_fields\[\d+\]/,
+								'medici_form_fields[' + index + ']'
+							);
+							$(this).attr('name', newName);
+						}
+					});
+			});
+		},
 
-    /**
-     * Copy shortcode to clipboard
-     */
-    copyShortcode: function (e) {
-      e.preventDefault();
+		/**
+		 * Copy shortcode to clipboard
+		 */
+		copyShortcode: function (e) {
+			e.preventDefault();
 
-      const $code = $("#mf-shortcode-code");
-      const text = $code.length ? $code.text() : $(this).text();
+			const $code = $('#mf-shortcode-code');
+			const text = $code.length ? $code.text() : $(this).text();
 
-      navigator.clipboard.writeText(text).then(function () {
-        const $btn = $("#mf-copy-shortcode");
-        const originalText = $btn.text();
-        $btn.text("Скопійовано!");
-        setTimeout(function () {
-          $btn.text(originalText);
-        }, 2000);
-      });
-    },
-  };
+			navigator.clipboard.writeText(text).then(function () {
+				const $btn = $('#mf-copy-shortcode');
+				const originalText = $btn.text();
+				$btn.text('Скопійовано!');
+				setTimeout(function () {
+					$btn.text(originalText);
+				}, 2000);
+			});
+		},
+	};
 
-  // Initialize on DOM ready
-  $(document).ready(function () {
-    MediciFormsBuilder.init();
-  });
+	// Initialize on DOM ready
+	$(document).ready(function () {
+		MediciFormsBuilder.init();
+	});
 })(jQuery);
--- plugins/medici-forms-pro/assets/js/settings.js
+++ plugins/medici-forms-pro/assets/js/settings.js
@@ -6,45 +6,45 @@
  */
 
 (function ($) {
-  "use strict";
+	'use strict';
 
-  $(document).ready(function () {
-    // Initialize color pickers
-    $(".medici-color-picker").wpColorPicker();
+	$(document).ready(function () {
+		// Initialize color pickers
+		$('.medici-color-picker').wpColorPicker();
 
-    // Conditional field visibility
-    function toggleRecaptchaFields() {
-      const isEnabled = $('input[name*="[enable_recaptcha]"]').is(":checked");
-      const $fields = $(
-        'input[name*="[recaptcha_site_key]"], input[name*="[recaptcha_secret_key]"], select[name*="[recaptcha_version]"], input[name*="[recaptcha_threshold]"]'
-      ).closest("tr");
+		// Conditional field visibility
+		function toggleRecaptchaFields() {
+			const isEnabled = $('input[name*="[enable_recaptcha]"]').is(':checked');
+			const $fields = $(
+				'input[name*="[recaptcha_site_key]"], input[name*="[recaptcha_secret_key]"], select[name*="[recaptcha_version]"], input[name*="[recaptcha_threshold]"]'
+			).closest('tr');
 
-      if (isEnabled) {
-        $fields.slideDown();
-      } else {
-        $fields.slideUp();
-      }
-    }
+			if (isEnabled) {
+				$fields.slideDown();
+			} else {
+				$fields.slideUp();
+			}
+		}
 
-    function toggleWebhookFields() {
-      const isEnabled = $('input[name*="[webhook_enabled]"]').is(":checked");
-      const $fields = $(
-        'input[name*="[webhook_url]"], select[name*="[webhook_method]"], textarea[name*="[webhook_headers]"]'
-      ).closest("tr");
+		function toggleWebhookFields() {
+			const isEnabled = $('input[name*="[webhook_enabled]"]').is(':checked');
+			const $fields = $(
+				'input[name*="[webhook_url]"], select[name*="[webhook_method]"], textarea[name*="[webhook_headers]"]'
+			).closest('tr');
 
-      if (isEnabled) {
-        $fields.slideDown();
-      } else {
-        $fields.slideUp();
-      }
-    }
+			if (isEnabled) {
+				$fields.slideDown();
+			} else {
+				$fields.slideUp();
+			}
+		}
 
-    // Initial state
-    toggleRecaptchaFields();
-    toggleWebhookFields();
+		// Initial state
+		toggleRecaptchaFields();
+		toggleWebhookFields();
 
-    // On change
-    $('input[name*="[enable_recaptcha]"]').on("change", toggleRecaptchaFields);
-    $('input[name*="[webhook_enabled]"]').on("change", toggleWebhookFields);
-  });
+		// On change
+		$('input[name*="[enable_recaptcha]"]').on('change', toggleRecaptchaFields);
+		$('input[name*="[webhook_enabled]"]').on('change', toggleWebhookFields);
+	});
 })(jQuery);
